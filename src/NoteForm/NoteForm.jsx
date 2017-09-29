8ece5b0a305267ab4d3b294051cb3e97e7b97aa9
Brian Donnoe <dunow33@hotmail.com>
2017-09-29 16:39:01 -0400

    more updates to todolist

---
+++ src/NoteForm/NoteForm.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './NoteForm.css';
+
+class NoteForm extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			newNoteContent: '',
+		};
+		this.handleUserInput = this.handleUserInput.bind(this);
+	}
+
+	handleUserInput(e){
+		this.setState({
+			newNoteContent: e.target.value,
+		})
+	}
+
+	render(){
+		return(
+			<div className="formWrapper">
+				<input className="noteInput"
+				placeholder="Write a new note....." 
+				value={this.state.newNoteContent}
+				onChange={this.handleUserInput} />
+				<button className="noteButton">Add Note</button>
+			</div>
+		)
+	}
+}
+
+export default NoteForm;
